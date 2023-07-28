59b6e3f0743848b6520b149ed256a191c41e8c33
Jason Gottlieb <gottlija@flip2.engr.oregonstate.edu>
2023-07-28 12:39:29 -0700

    Step 0 and 1 of Github Tutorial

---
+++ app.js
@@ -0,0 +1,61 @@
+// Citation for the following code:
+// Date: 06/26/2023
+// Copied from
+// OSU Course CS340 Intro to Databases Activity 2. Modified slightly to have a unique port
+// Source URL: https://canvas.oregonstate.edu/courses/1922991/assignments/9287060?module_item_id=23329570 
+
+/*
+    SETUP
+*/
+// Express
+var express = require('express');   // We are using the express library for the web server
+var app = express();            // We need to instantiate an express object to interact with the server in our code
+PORT = 60350;                 // Set a port number at the top so it's easy to change in the future
+
+
+
+// Database
+var db = require('./db-connector')
+
+
+
+/*
+    ROUTES
+*/
+app.get('/', function (req, res) {
+    // Define our queries
+    query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+    query4 = 'SELECT * FROM diagnostic;';
+
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+    // DROP TABLE...
+    db.pool.query(query1, function (err, results, fields) {
+
+        // CREATE TABLE...
+        db.pool.query(query2, function (err, results, fields) {
+
+            // INSERT INTO...
+            db.pool.query(query3, function (err, results, fields) {
+
+                // SELECT *...
+                db.pool.query(query4, function (err, results, fields) {
+
+                    // Send the results to the browser
+                    let base = "<h1>MySQL Results:</h1>"
+                    res.send(base + JSON.stringify(results));
+                });
+            });
+        });
+    });
+});
+
+
+/*
+    LISTENER
+*/
+app.listen(PORT, function () {            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
---
+++ db-connector.js
@@ -0,0 +1,21 @@
+// Citation for the following functions:
+// Date: 06/26/2023
+// Copied from
+// OSU Course CS340 Intro to Databases Activity 2. Modified slightly to include my credentials
+// Source URL: https://canvas.oregonstate.edu/courses/1922991/assignments/9287060?module_item_id=23329570 
+
+// Get an instance of mysql we can use in the app
+var mysql = require('mysql')
+
+// Create a 'connection pool' using the provided credentials
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: 'classmysql.engr.oregonstate.edu',
+    user: 'cs340_gottlija',
+    password: '0473',
+    database: 'cs340_gottlija'
+})
+
+// Export it for use in our application
+module.exports.pool = pool;
+
