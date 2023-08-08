84a414f7dd72a980d2b33c2f64811130d8032ca6
Matthew French <frencmat@flip2.engr.oregonstate.edu>
2023-08-08 11:20:04 -0700

    first commit

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 This is a test change from Jason on the OSU Flip2 Server
 
 This is a test change from Matt on the OSU Flip2 Server
+
+latestMattBranch test
--- app.js
+++ app.js
@@ -10,10 +10,16 @@
 // Express
 var express = require('express');   // We are using the express library for the web server
 var app = express();            // We need to instantiate an express object to interact with the server in our code
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
+PORT = 60351;                 // Set a port number at the top so it's easy to change in the future
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-PORT = 60353;                 // Set a port number at the top so it's easy to change in the future
+PORT = 60352;                 // Set a port number at the top so it's easy to change in the future
 
 
 
@@ -64,7 +70,9 @@ app.get('/Teams.hbs', function (req, res) {
 
     db.pool.query(query1, function (error, rows, fields) {    // Execute the query
 
-        res.render('Teams', { data: rows });                  // Render the index.hbs file, and also send the renderer
+        let teams = rows;
+
+        return res.render('Teams', { data: teams });                  // Render the index.hbs file, and also send the renderer
     })                                                      // an object where 'data' is equal to the 'rows' we
 });                                                         // received back from the query
 
@@ -94,7 +102,7 @@ app.post('/add_team', function (req, res) {
     // Capture NULL values
 
     // Create the query and run it on the database
-    query1 = `INSERT INTO Teams (team_name, team_country, car_model) VALUES ('${data.team_name}', '${data.team_country}', '${data.car_model}')`;
+    query1 = `INSERT INTO Teams (team_name, team_country, car_model) VALUES ('${data['team_name']}', '${data['team_country']}', '${data['car_model']}')`;
     db.pool.query(query1, function (error, rows, fields) {
 
         // Check to see if there was an error
@@ -125,27 +133,60 @@ app.post('/add_team', function (req, res) {
     })
 });
 
-app.delete('/delete_team', function (req, res, next) {
+
+
+
+app.delete('/delete-team-ajax/', function(req,res,next){
     let data = req.body;
     let teamID = parseInt(data.team_id);
     let deleteTeam = `DELETE FROM Teams WHERE team_id = ?`;
+    
+          // Run the 1st query
+          db.pool.query(deleteTeam, [teamID], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
+              else
+              {
+
+              }
+  })});
 
 
-    // Run the 1st query
-    db.pool.query(deleteTeam, [teamID], function (error, rows, fields) {
-        if (error) {
+  app.put('/put-team-ajax', function(req,res,next){
+    let data = req.body;
+    
+    let team_id = parseInt(data.team_id)
+    let team_name = data.team_name;
+    let team_country = data.team_country;
+    let team_model = data.team_model;
+  
+    let queryUpdateTeam = `UPDATE Teams SET team_name = ?, team_country = ?, team_model = ? WHERE Teams.team_id = ?`;
 
-            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
-            console.log(error);
-            res.sendStatus(400);
-        }
+    
+  
+          // Run the 1st query
+          db.pool.query(queryUpdateTeam, [team_id, team_name, team_country, team_model], function(error, rows, fields){
+              if (error) {
+  
+              // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+              console.log(error);
+              res.sendStatus(400);
+              }
+  
 
-        else {
-            res.sendStatus(204);
-        }
-    })
-});
+  })});
 
+// app.get('/', function (req, res) {
+//     // Define our queries
+//     query1 = 'DROP TABLE IF EXISTS diagnostic;';
+//     query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+//     query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+//     query4 = 'SELECT * FROM diagnostic;';
 
 app.put('/update_team', function (req, res, next) {
     let data = req.body;
