ab2305993856facacdf4ef84c09dd8a07b5c0181
Jason Gottlieb <gottlija@flip2.engr.oregonstate.edu>
2023-08-08 11:06:30 -0700

    made Teams_team_id nullable

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = express();            // We need to instantiate an express object to i
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-PORT = 60352;                 // Set a port number at the top so it's easy to change in the future
+PORT = 60353;                 // Set a port number at the top so it's easy to change in the future
 
 
 
